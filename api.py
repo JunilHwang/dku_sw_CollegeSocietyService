28176d4bf4a8de86275b7e6c6ece2ba0a80147f6
farka-k <farka.k44@gmail.com>
2018-05-13 16:36:51 +0900

    pw암호화, HTTP Basic Auth

--- api.py
+++ api.py
@@ -1,18 +1,35 @@
 #api.py
 '''
-<To Do List> - 18.05.11
-user입력,변경시 validation추가
+<To Do List> - 18.05.13
 cms,meta_data,file 테이블에 대한 api추가
-http인증
+HTTP인증 - 추가 테스트 필요
 파일처리(?)
 '''
-from flask import jsonify, make_response
+'''
+<Guide>
+자원에 대한 수정은 patch메소드로 수정할 attribute만 전송
+인증이 필요한 경우 authentication header에 인증방식과 정보를 설정
+'''
+from flask import jsonify, g        #g:flask global object
 from flask_restful import Api,Resource,reqparse
 from models import *
+from flask_httpauth import HTTPBasicAuth        #for http authentication
 from sqlalchemy.exc import SQLAlchemyError
 import status
 
 api=Api()
+auth=HTTPBasicAuth()
+
+@auth.verify_password
+def verify_user_password(id,pw):
+    user=member.query.filter_by(id=id).first()
+    if not user or not user.verify_password(pw):
+        return False
+    g.user=user
+    return True
+class AuthRequiredResource(Resource):
+    method_decorators=[auth.login_required]
+
 class UserResource(Resource):
     def post(self): #add new user
         parser=reqparse.RequestParser()
@@ -35,19 +52,26 @@ class UserResource(Resource):
         _email=args['email']
         _nickname=args['nickname']
         
+        exist_id=member.query.filter_by(id=_id).first()
+        if exist_id is not None:    #입력 id가 이미 존재할때
+            return {"error": "User with the same ID already exists"}, status.HTTP_400_BAD_REQUEST
         try:
-            new_member=member(_id,_pw,_name,_college,_major,_undergrad,_email,_nickname)
-            new_member.add(new_member)
-
+            new_member=member(_id,_name,_college,_major,_undergrad,_email,_nickname)
+            error_message, pw_ok = \
+                new_member.check_password_strength_and_hash_if_ok(_pw) #패스워드 유효성 체크, 만족시 해시 함수 적용.
+            if pw_ok:
+                new_member.add(new_member)      #member추가
+                query=member.query.get(new_member.idx)  #추가된 새 member에 대한 정보를 받아옴
+                result=query.as_dict()                  #<---필요한 attribute만 남기고 리턴할 것
+                return result, status.HTTP_201_CREATED
+            else:
+                return {"error":error_message}, status.HTTP_400_BAD_REQUEST
         except SQLAlchemyError as e:
             db.session.rollback()
             resp={"error":str(e)}
-            return jsonify(resp), status.HTTP_400_BAD_REQUEST
-        query=member.query.get(new_member.idx)
-        result=query.as_dict()
-
-        return jsonify(result), status.HTTP_201_CREATED
-   
+            return resp, status.HTTP_400_BAD_REQUEST
+    
+    @auth.login_required        #admin만 접근 가능해야함
     def get(self):      #show all users
         query=member.query.all()
         result=many_returns(query)
@@ -56,26 +80,32 @@ class UserResource(Resource):
         return jsonify(result)
 
 
-class ProfileResource(Resource):
+class ProfileResource(AuthRequiredResource):    #해당 클래스 자원은 모두 인증이 필요
     def get(self,idx): #show user info
         current_user=member.query.get_or_404(idx)
-        result=current_user.as_dict()
-        result['regdate']=str(result['regdate'])
-        return jsonify(result)
+        if g.user.id==current_user.id:
+            result=current_user.as_dict()
+            result['regdate']=str(result['regdate'])
+            del result['pw']        #응답에서 password 항목 삭제
+            return jsonify(result)
+        else:
+            return {'error':'Not allowed to access this resource'}, status.HTTP_403_FORBIDDEN
 
     def patch(self, idx): #edit user info
         current_user=member.query.get_or_404(idx)
+        if g.user.id is not current_user.id:    
+            return {'error':'Not allowed to access this resource'}, status.HTTP_403_FORBIDDEN
 
         parser=reqparse.RequestParser()
         #parsing list중 필요없는것 삭제할 것
-        parser.add_argument('id', type=str, location='form')
-        parser.add_argument('pw', type=str, location='form')
-        parser.add_argument('name', type=str, location='form')
-        parser.add_argument('college', type=str, location='form')
-        parser.add_argument('major', type=str, location='form')
-        parser.add_argument('undergrad_number', type=int, location='form')
-        parser.add_argument('email', type=str, location='form')
-        parser.add_argument('nickname', type=str, location='form')
+        parser.add_argument('id', type=str)
+        parser.add_argument('pw', type=str)
+        parser.add_argument('name', type=str)
+        parser.add_argument('college', type=str)
+        parser.add_argument('major', type=str)
+        parser.add_argument('undergrad_number', type=int)
+        parser.add_argument('email', type=str)
+        parser.add_argument('nickname', type=str)
         args=parser.parse_args()
 
         _id=args['id']
@@ -89,7 +119,10 @@ class ProfileResource(Resource):
 
         try:
             if _pw is not None:
-                current_user.pw=_pw
+                error_message, pw_ok = \
+                current_user.check_password_strength_and_hash_if_ok(_pw)    #패스워드 유효성체크 및 암호화
+                if pw_ok is False:
+                    return {"error":error_message}, status.HTTP_400_BAD_REQUEST
             if _college is not None:
                 current_user.college=_college
             if _major is not None:
@@ -99,14 +132,17 @@ class ProfileResource(Resource):
             if _nickname is not None:
                 current_user.nickname=_nickname
             current_user.update()
-            return jsonify({'idx':self.get(idx)})
+            return {"idx":current_user.idx}
+
         except SQLAlchemyError as e:
             db.session.rollback()
             resp={"error":str(e)}
-            return resp,status.HTTP_400_BAD_REQUEST
+            return resp, status.HTTP_400_BAD_REQUEST
 
     def delete(self,idx):
         current_user=member.query.get_or_404(idx)
+        if g.user.id is not current_user.id:        #인증과 삭제할 유저의 id가 일치하지 않을 때
+            return {"error":"Not allowed to access this resource"}, status.HTTP_403_FORBIDDEN
         try:
             current_user.delete(current_user)
             return True, status.HTTP_204_NO_CONTENT
@@ -116,30 +152,29 @@ class ProfileResource(Resource):
             return resp, status.HTTP_401_UNAUTHORIZED
 
 class CategoryResource(Resource):
-    def get(self):
+    def get(self):      #category정보를 리턴
         query=category.query.all()
         result=many_returns(query)
-        
-        return result
+        return jsonify(result)
 
 class BoardResource(Resource):
     def get(self, cate): #show list of post
         ctg=category.query.filter_by(idx=cate).first()
         if ctg is None:
-            return {"error":"Category doesn't exist."}
+            return {"error":"Category doesn't exist."}, status.HTTP_404_NOT_FOUND
         query=board.query.filter_by(category=ctg.idx).order_by(board.reg_date).all()
         result=many_returns(query)
         for x in result:
             x['reg_date']=str(x['reg_date'])
         return jsonify(result)
 
+    @auth.login_required
     def post(self, cate): #add new post board
         ctg=category.query.filter_by(idx=cate).first()
         if ctg is None:
-            return {"error":"Category doesn't exist."}, status.HTTP_406_NOT_ACCEPTABLE
+            return {"error":"Category doesn't exist."}, status.HTTP_404_NOT_FOUND
 
         parser=reqparse.RequestParser()
-        parser.add_argument('writer', type=int, required=True)
         parser.add_argument('parent', type=int, required=True)
         parser.add_argument('od', type=int, required=True)
         parser.add_argument('depth', type=int, required=True)
@@ -147,14 +182,12 @@ class BoardResource(Resource):
         parser.add_argument('content', type=str, required=True)
         args=parser.parse_args()
 
-        _writer=args['writer']
+        _writer=g.user.idx
         _parent=args['parent']
         _od=args['od']
         _depth=args['depth']
         _subject=args['subject']
         _content=args['content']
-        
-        mem=member.query.filter_by(idx=_writer).first()
         
         try:
             new_post=board(cate,_writer,_parent,_od,_depth,_subject,_content)
@@ -179,10 +212,13 @@ class PostResource(Resource):
         result['reg_date']=str(result['reg_date'])
         return result
 
+    @auth.login_required
     def patch(self,cate,idx): #edit post
         current_post=board.query.filter_by(category=cate,idx=idx).first()
         if current_post is None:
             return {"error":"Resource Not Founded"}, status.HTTP_404_NOT_FOUND
+        if g.user.idx is not current_post.writer:       #인증과 수정할 post의 작성자가 일치하지 않을 때
+            return {"error":'Not allowed to access this resource'}, status.HTTP_403_FORBIDDEN
 
         parser=reqparse.RequestParser()
         parser.add_argument('subject', type=str, required=True)
@@ -200,10 +236,13 @@ class PostResource(Resource):
             resp={"error":str(e)}
             return resp, status.HTTP_400_BAD_REQUEST
 
+    @auth.login_required
     def delete(self,cate,idx): #delete post
         current_post=board.query.filter_by(category=cate,idx=idx).first()
         if current_post is None:
             return {"error":"Resource Not Founded"}, status.HTTP_404_NOT_FOUND
+        if g.user.idx is not current_post.writer:       #인증과 삭제할 post의 작성자가 일치하지 않을때
+            return {"error":"Not allowed to access this resource"}, status.HTTP_403_FORBIDDEN
         try:
             current_post.delete(current_post)
             return True, status.HTTP_204_NO_CONTENT
@@ -216,7 +255,7 @@ class CommentListResource(Resource):
     def get(self,post_idx):  #show comment list of the post
         pst=board.query.filter_by(idx=post_idx).first()
         if pst is None:
-            return {"error":"Post doesn't exist"}, status.HTTP_406_NOT_ACCEPTABLE
+            return {"error":"Post doesn't exist"}, status.HTTP_404_NOT_FOUND
         comments=comment.query.filter_by(bidx=pst.idx).all()
         result=many_returns(comments)
         
@@ -224,18 +263,18 @@ class CommentListResource(Resource):
             x['date']=str(x['date'])
         return jsonify(result)
 
+    @auth.login_required
     def post(self,post_idx): #add new comment on post
         pst=board.query.filter_by(idx=post_idx).first()
         if pst is None:
-            return {"error":"Post doesn't exist"}, status.HTTP_406_NOT_ACCEPTABLE
+            return {"error":"Post doesn't exist"}, status.HTTP_404_NOT_FOUND
         parser=reqparse.RequestParser()
-        parser.add_argument('writer', type=int, required=True)
         parser.add_argument('od', type=int, required=True)
         parser.add_argument('depth', type=int, required=True)
         parser.add_argument('content', type=str, required=True)
         args=parser.parse_args()
 
-        _writer=args['writer']
+        _writer=g.user.idx
         _od=args['od']
         _depth=args['depth']
         _content=args['content']
@@ -255,9 +294,11 @@ class CommentListResource(Resource):
         return jsonify(result), status.HTTP_201_CREATED
 
 class CommentResource(Resource):
+    @auth.login_required
     def patch(self, comment_idx): #edit comment
         current_comment=comment.query.get_or_404(comment_idx)
-        
+        if g.user.idx is not current_comment.writer:    #인증과 수정할 comment의 작성자가 일치하지 않을때
+            return {"error":"Not allowed to access this resource"}, status.HTTP_403_FORBIDDEN
         parser=reqparse.RequestParser()
         parser.add_argument('content', type=str, required=True)
         args=parser.parse_args()
@@ -270,9 +311,12 @@ class CommentResource(Resource):
             db.session.rollback()
             resp={"error":str(e)}
             return resp,status.HTTP_400_BAD_REQUEST
-
+    
+    @auth.login_required
     def delete(self, comment_idx):   #delete comment
         current_comment=comment.query.get_or_404(comment_idx)
+        if g.user.idx is not current_comment.writer:        #인증과 삭제할 comment의 작성자가 일치하지 않을때
+            return {"error":"Not allowed to access this resource"}, status.HTTP_403_FORBIDDEN
         try:
             current_comment.delete(current_comment)
             return True, status.HTTP_204_NO_CONTENT
@@ -280,8 +324,6 @@ class CommentResource(Resource):
             db.session.rollback()
             resp={"error":str(e)}
             return resp, status.HTTP_401_UNAUTHORIZED
-
-
 
 api.add_resource(UserResource,'/member')
 api.add_resource(ProfileResource,'/member/<int:idx>')
--- models.py
+++ models.py
@@ -2,10 +2,11 @@
 #models mapping to database
 
 from flask_sqlalchemy import SQLAlchemy
-
+from passlib.apps import custom_app_context as password_context     #for password hashing
+import re       #for regular expression
 db=SQLAlchemy()
 
-def many_returns(query):
+def many_returns(query):    #쿼리 응답이 여러개의 튜플을 리턴할 때 serialization
     tuple_list=[]
     for tuple in query:
         tuple_list.append(tuple.as_dict())
@@ -20,13 +21,14 @@ class AddUpdateDelete():
     def delete(self,resource):
         db.session.delete(resource)
         return db.session.commit()
-    def as_dict(self):
+    def as_dict(self):      #쿼리결과를 dictionary로 변환
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+#class이름과 db table이름을 일치시킨다. 일치하지 않을땐 __tablename__="테이블명"으로 매칭
 class member(db.Model, AddUpdateDelete):
     idx=db.Column(db.Integer, primary_key=True)
     id=db.Column(db.String(16), unique=True, nullable=False)
-    pw=db.Column(db.String(255), nullable=False)
+    pw=db.Column(db.String(255), nullable=False)    #hashed password
     name=db.Column(db.String(20), nullable=False)
     college=db.Column(db.String(20), nullable=False)
     major=db.Column(db.String(20), nullable=False)
@@ -36,9 +38,27 @@ class member(db.Model, AddUpdateDelete):
     regdate=db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
     level=db.Column(db.Integer, default=1)
 
-    def __init__(self,id,pw,name,college,major,undergrad_number,email,nickname):
+    def verify_password(self, password):
+        return password_context.verify(password, self.pw)
+
+    #패스워드의 유효성을 검사하고 모두 만족시 해시 암호화 한다(64bit-SHA512,32bit-SHA256)
+    #처리에 시간이 오래걸릴 경우 필요 라운드 수를 줄여야 함
+    def check_password_strength_and_hash_if_ok(self, password):
+        if len(password)<8:
+            return 'The password is too short.', False
+        if len(password)>16:
+            return 'The password is too long.', False
+        if re.search(r'[a-zA-Z]', password) is None:
+            return 'The password must include at least one alphabet', False
+        if re.search(r'\d', password) is None:
+            return 'The password must include at least one number', False
+        if re.search(r"[!@#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None:
+            return 'The password must include at least one symbol', False
+        self.pw=password_context.encrypt(password)
+        return '', True
+
+    def __init__(self,id,name,college,major,undergrad_number,email,nickname):
         self.id=id
-        self.pw=pw
         self.name=name
         self.college=college
         self.major=major
@@ -46,8 +66,8 @@ class member(db.Model, AddUpdateDelete):
         self.email=email
         if nickname:
             self.nickname=nickname
-        else:
-            self.nickname=id
+        else:   #닉네임이 None이면 이름을 닉네임으로 설정.
+            self.nickname=name
 
 class board(db.Model,AddUpdateDelete):
     idx=db.Column(db.Integer, primary_key=True)
